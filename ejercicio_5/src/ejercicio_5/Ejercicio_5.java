a6aef702a99a61c32957d72297742c2ca20ac2e3
Aaron Vigil <aaronvigil96@gmail.com>
2023-03-19 20:23:37 -0300

    Exercise five finished

--- ejercicio_5/src/ejercicio_5/Ejercicio_5.java
+++ ejercicio_5/src/ejercicio_5/Ejercicio_5.java
@@ -8,11 +8,36 @@ public class Ejercicio_5 {
 
     public static void main(String[] args) {
         int[][] matriz = new int[4][4];
-    }
-    public static void llenarMatriz(int[][] matriz){
         for (int i = 0; i < matriz.length; i++) {
-            
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = (int) (Math.random() * 9);
+            }
+        }
+        
+        System.out.println("Matriz original: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+                
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("Matriz trasversal:");
+        int auxiliar;   
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < i; j++) {
+                auxiliar = matriz[i][j];
+                matriz[i][j] = matriz[j][i];
+                matriz[j][i] = auxiliar;
+            }
+        }
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
         }
     }
-    
 }
